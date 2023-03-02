d74f16a77849934536ed2ec01eb0ce0dd4e64327
voyager <cdspacenoob@gmail.com>
2023-03-02 17:54:25 +0900

    how to using Link from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
 
-const routeDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-);
-
-const router = createBrowserRouter(routeDefinition);
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <Products /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <Products /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
