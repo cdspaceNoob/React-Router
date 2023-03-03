2c91dfd06925515c2f869c728afe546d76e1588f
voyager <cdspacenoob@gmail.com>
2023-03-03 15:06:28 +0900

    useParams provide dynamic url

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,11 @@
+import { Fragment } from "react";
+
 const Products = () => {
-  return <h1>This is Products Page.</h1>;
+  return (
+    <Fragment>
+      <h1>This is Products Page.</h1>
+    </Fragment>
+  );
 };
 
 export default Products;
