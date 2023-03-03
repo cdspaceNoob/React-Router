9d08bb835984c2320048ccce10e3ad0c36602750
voyager <cdspacenoob@gmail.com>
2023-03-03 15:23:56 +0900

    use map then generate list of Links

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,23 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const DUMMY_PRODUCT = [
+  { id: "p1", title: "Product 01" },
+  { id: "p2", title: "Product 02" },
+  { id: "p3", title: "Product 03" },
+];
 
 const Products = () => {
   return (
     <Fragment>
       <h1>This is Products Page.</h1>
+      <ul>
+        {DUMMY_PRODUCT.map((item) => (
+          <li key={item.id}>
+            <Link to={`/products/${item.id}`}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
     </Fragment>
   );
 };
